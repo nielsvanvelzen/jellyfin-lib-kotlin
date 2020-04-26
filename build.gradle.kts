dd19f0c7fc3c658dfdc98e7d05cb89853ce64381
Niels van Velzen <git@ndat.nl>
2020-04-26 12:53:32 +0200

    Add github workflow for building and publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,43 @@
+plugins {
+	id("maven-publish")
+}
+
 buildscript {
 	repositories {
 		google()
 		jcenter()
 	}
+
 	dependencies {
 		classpath("com.android.tools.build:gradle:3.6.3")
 		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+	}
+}
+
+
+subprojects {
+	apply(plugin = "maven-publish")
+
+	group = "org.jellyfin"
+	version = "1.0.0"
+
+	publishing {
+		repositories {
+			maven {
+				url = uri("https://api.bintray.com/maven/nielsvanvelzen/jellyfin-lib-kotlin/org.jellyfin;publish=0")
+
+				credentials {
+					username = project.findProperty("bintray.user") as String? ?: System.getenv("BINTRAY_USER")
+					password = project.findProperty("bintray.key") as String? ?: System.getenv("BINTRAY_KEY")
+				}
+			}
+		}
+
+		publications {
+			create<MavenPublication>("maven") {
+				from(components.findByName("kotlin") ?: components.findByName("java"))
+			}
+		}
 	}
 }
 
